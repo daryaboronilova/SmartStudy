a3994d0eeadc864b621492a5c9d8ee3dabd15a4c
daryaboronilova <148972942+daryaboronilova@users.noreply.github.com>
2023-10-31 17:40:04 +0300

    123
    123

--- pycharm/pycharm/Django1/Djangoapp/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class DdjandoappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Djangoapp'
-    verbose_name = 'Новостной портал'
